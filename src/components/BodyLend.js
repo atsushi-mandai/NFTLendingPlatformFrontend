19198076d3c152b5d75a422aed5f382add283922
Atsushi Mandai <atsushi.mandai.1991@gmail.com>
2022-08-09 20:23:17 +0900

    initial commit

--- src/components/BodyLend.js
+++ src/components/BodyLend.js
@@ -1,14 +1,31 @@
 import React, { useRef } from "react";
-import { abi } from "./scripts/Contract";
 import { CheckNFTBalance } from "./scripts/CheckNFTBalance";
+import axios from "axios";
 
 const component = ({ web3, app, account }) => {
   const nftAddress = useRef();
+  const nftId = useRef();
 
   const handlerSearch = async () => {
-    const bool = await CheckNFTBalance(web3, account, nftAddress.current.value);
-    if (bool == true) {
-      window.alert("Seems like you have an NFT!");
+    const nftContract = await CheckNFTBalance(
+      web3,
+      account,
+      nftAddress.current.value,
+      nftId.current.value
+    );
+    if (nftContract != false) {
+      const tokenURI = await nftContract.methods
+        .tokenURI(nftId.current.value)
+        .call();
+      console.log(tokenURI);
+      try {
+        //Having a hard time finding NFT contract with tokenURI on BNB testnet.
+        const response = await axios.get(tokenURI);
+        const metadata = response.data;
+        console.log(metadata);
+      } catch (error) {
+        console.log("caught error");
+      }
     }
   };
 
@@ -17,8 +34,15 @@ const component = ({ web3, app, account }) => {
       <h2>Lend NFT to earn ETH.</h2>
       <h3>Your NFTs</h3>
       <p>Enter the contract address of your ERC4907 token.</p>
-      <input type="text" ref={nftAddress}></input>
-      <button onClick={handlerSearch}>Search</button>
+      <p className="inputField">
+        <input type="text" ref={nftAddress}></input>
+      </p>
+      <p className="inputField">
+        <input type="text" ref={nftId}></input>
+      </p>
+      <p>
+        <button onClick={handlerSearch}>Search</button>
+      </p>
       <h3>Staked NFTs</h3>
     </div>
   );
--- src/components/scripts/CheckNFTBalance.js
+++ src/components/scripts/CheckNFTBalance.js
@@ -1,32 +1,30 @@
 import { abi } from "./Contract";
 
-const checkNFTBalance = async (web3, account, address) => {
-  if (account > 0) {
-    const checkAddress = await web3.utils.isAddress(address);
-    if (checkAddress == false) {
-      window.alert("Enter the right address of your ERC4907 token.");
-      return false;
-    } else {
-      try {
-        //const address = nftAddress.current.value;
-        const nftContract = await new web3.eth.Contract(abi, address);
-        const balance = await nftContract.methods.balanceOf(account).call();
-        if (balance > 0) {
-          return true;
-        } else {
-          window.alert("Couldn't find your ERC4907 token with this contract.");
-          return false;
-        }
-      } catch (error) {
-        window.alert(
-          "This address doesn't seem to be an ERC4907 token contract."
-        );
-        return false;
-      }
-    }
-  } else {
+const checkNFTBalance = async (web3, account, nftAddress, nftId) => {
+  let nftContract;
+  let owner;
+  if (account == null) {
     window.alert("Connect your wallet.");
     return false;
+  } else if (web3.utils.isAddress(nftAddress) == false) {
+    window.alert("Enter the right address of your ERC4907 Token.");
+    return false;
+  } else {
+    try {
+      nftContract = await new web3.eth.Contract(abi, nftAddress);
+      owner = await nftContract.methods.ownerOf(nftId).call();
+    } catch (error) {
+      window.alert(
+        "This address doesn't seem to be an ERC4907 token contract."
+      );
+      return false;
+    }
+    if (owner == account) {
+      return nftContract;
+    } else {
+      window.alert("This token doesn't belong to your wallet.");
+      return false;
+    }
   }
 };
 
